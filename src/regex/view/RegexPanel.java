718e4d3b827ad2a4463f344245e943744749d62f
smeth194243 <seth.r.m1@gmail.com>
2017-02-22 12:56:26 -0700

    updated panel and stuff

--- src/regex/view/RegexPanel.java
+++ src/regex/view/RegexPanel.java
@@ -18,6 +18,27 @@ public class RegexPanel extends JPanel
 	private JTextField lastName;
 	private JTextField phoneNumber;
 	private JTextField validEmail;
-	
+	private JLabel fNLabel;
+	private JLabel lNLabel;
+	private JLabel pNLabel;
+	private JLabel vELabel;
+	private JButton doesTalking;	
+
+
+	public RegexPanel(RegexController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		baseLayout = new SpringLayout();
+		firstName = new JTextField(25);
+		lastName = new JTextField(25);
+		phoneNumber = new JTextField(25);
+		validEmail = new JTextField(25);
+		fNLabel = new JLabel("First Name");
+		lNLabel = new JLabel("Last Name");
+		pNLabel = new JLabel("Phone number");
+		vELabel = new JLabel("Valid Email");
+		doesTalking = new JButton();
+	}
 	
 }
