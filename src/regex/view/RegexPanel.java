14bf87a5a861ef2c75e9bdd5c9c68a175727e30b
smeth194243 <seth.r.m1@gmail.com>
2017-02-24 13:18:18 -0700

    setupLayout

--- src/regex/view/RegexPanel.java
+++ src/regex/view/RegexPanel.java
@@ -33,12 +33,18 @@ public class RegexPanel extends JPanel
 		firstName = new JTextField(25);
 		lastName = new JTextField(25);
 		phoneNumber = new JTextField(25);
+		
 		validEmail = new JTextField(25);
 		fNLabel = new JLabel("First Name");
+		
 		lNLabel = new JLabel("Last Name");
+		
 		pNLabel = new JLabel("Phone number");
+		
 		vELabel = new JLabel("Valid Email");
+		
 		doesTalking = new JButton("Validate information");
+	
 		
 		this.setupPanel();
 		this.setupLayout();
@@ -47,6 +53,7 @@ public class RegexPanel extends JPanel
 	
 	public void setupPanel()
 	{
+		this.setLayout(baseLayout);
 		this.add(doesTalking);
 		this.add(fNLabel);
 		this.add(firstName);
@@ -60,7 +67,25 @@ public class RegexPanel extends JPanel
 	
 	public void setupLayout()
 	{
-	
+		baseLayout.putConstraint(SpringLayout.EAST, phoneNumber, 0, SpringLayout.EAST, firstName);
+		baseLayout.putConstraint(SpringLayout.WEST, validEmail, 0, SpringLayout.WEST, fNLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, fNLabel, 234, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, fNLabel, -153, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, lastName, 0, SpringLayout.WEST, fNLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstName, 6, SpringLayout.SOUTH, fNLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, firstName, 0, SpringLayout.WEST, fNLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, fNLabel, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, lastName, 6, SpringLayout.SOUTH, lNLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, lNLabel, 6, SpringLayout.SOUTH, firstName);
+		baseLayout.putConstraint(SpringLayout.WEST, lNLabel, 0, SpringLayout.WEST, fNLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, phoneNumber, 6, SpringLayout.SOUTH, pNLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, pNLabel, 0, SpringLayout.WEST, fNLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, validEmail, 6, SpringLayout.SOUTH, vELabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, vELabel, 10, SpringLayout.SOUTH, phoneNumber);
+		baseLayout.putConstraint(SpringLayout.EAST, vELabel, 0, SpringLayout.EAST, lNLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, doesTalking, 95, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, pNLabel, 4, SpringLayout.NORTH, doesTalking);
+		baseLayout.putConstraint(SpringLayout.WEST, doesTalking, 22, SpringLayout.WEST, this);
 	}
 	
 	public void setupListeners()
